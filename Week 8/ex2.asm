f384f990c59b76c7a8caf4439b34a581673d8a84
IVIosab <mosab.f.r@gmail.com>
2020-10-22 21:16:27 +0300

    corrected week8 ex2

--- Week 8/ex2.asm
+++ Week 8/ex2.asm
@@ -4,12 +4,15 @@ msg2: .asciiz "\nThe sum of all natural numbers upto n is : "
 input: .word 0
 answer: .word 0
 .text 
+
+
 li $v0,4
 la $a0, msg1 #outputting the msg "Enter n : "
 syscall
 
 li $v0,5 #taking input
 syscall
+sw $v0, input
 
 lw $a0,input
 jal rec #call the function
@@ -20,27 +23,26 @@ la $a0, msg2 #outputting the msg "\nThe sum of all natural numbers upto n is : "
 syscall
 
 li $v0, 1
-la $a0,answer #outputting the answer
+lw $a0,answer #outputting the answer
 syscall
 
 li $v0, 10
-syscall 
-
+syscall
+ 
 rec : 
-	addi $sp,$sp, -8
-	sw $a0, 0($sp)
-	sw $ra, 4($sp)
-	
-	li $v0,0
-	beq $a0,$zero,done # go to done if we've reached 0
-    	
-    	addi $a0,$a0,-1
+	subu $sp,$sp,8
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	li $v0, 0
+	beq $a0,$zero,done  # go to done if we've reached 0
+    	move $s0, $a0
+    	subi $a0, $a0, 1 
     	jal rec
-    	lw $a0, 0($sp)
     	
-    	add $v0, $v0,$a0
+    	add $v0,$s0,$v0
+    	
 done : 
-	lw $a0,0($sp)
-    	lw $ra,4($sp)
-    	addi $sp,$sp,8
-    	jr $ra	
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	addu $sp, $sp, 8
+	jr $ra
